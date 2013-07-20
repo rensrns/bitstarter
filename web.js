7efdb8cfbb3821258e6f292ae2f23c1e1253bf47
rensrns <srns2000-yahoo.com>
2013-07-20 05:42:42 +0000

    Updated web.js to upload images

--- web.js
+++ web.js
@@ -2,12 +2,13 @@ var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
 var buffer = new Buffer(fs.readFileSync("index.html"));
+app.use(express.static(__dirname + '/images'));
 
 app.get('/', function(request, response) {
   response.send(buffer.toString("utf-8"));
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
